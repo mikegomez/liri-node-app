e6360726204f76e2c011c9c0b0cb5bdfa0863f7a
Mike Gomez <mikegomez21@hotmail.com>
2018-04-23 20:45:48 -0500

    Liri-node-app

---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log('this is loaded');
+
+  var twitterKeys = {
+
+  consumer_key: 'fIFEv5t5kS2PNBl0jyW7wnvum',
+	consumer_secret: '4XJL7kMvPxRIBUTEnNcraG6FmKi6VvOa2YOIBjJVDeeAzGfM0I',
+	access_token_key: '2198928756-zPMgPKUMT33BwEb3PV4F2iPuYs0XMSHbBC3k0Nd',
+  access_token_secret: 'py4EIczclOdNNS7pXvBEaZ4LYt9alJQge1Q09aVu6G6Pd',
+ };
+  
+  var spotifyKeys = {
+    id: '6138fa3b06904163aeb8484aa3e571da',
+    secret: 'dddd1f154aca4a238bd05ce89783bf28',
+  };
+  
+  module.exports = { twitterKeys, spotifyKeys };
